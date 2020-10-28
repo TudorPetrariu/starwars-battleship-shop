ac09604332e222a6c60cf2d83b4d49091dce5e8a
Tudor <t.petrariu@jobsrepublic.nl>
2020-10-28 19:35:00 +0100

    started working on the design of the details page component

--- src/components/starships/StarShipDetailsPage.vue
+++ src/components/starships/StarShipDetailsPage.vue
@@ -1,6 +1,45 @@
 <template>
   <div>
     <app-nav />
+    <header>
+      <h1>Welcome to StarshipList Details Page</h1>
+    </header>
+    <section class="ship-details">
+      <div id="trigger-magic-scroll" class="ship-title">
+        <h2>
+          {{ getbattleShipInfo.model }}
+          <b-badge class="text-danger" variant="primary" pill>{{
+            getbattleShipMovies.length
+          }}</b-badge>
+        </h2>
+      </div>
+      <div class="about-ship-pages">
+        <div
+          v-for="item in getbattleShipMovies"
+          :key="item.name"
+          class="ship-movie-page"
+        >
+          <h2>
+            Episode {{ item.episode_id }}
+            <br />
+            {{ item.title }}
+            <br />
+
+            <small>Year {{ item.release_date }}</small>
+            <br />
+            <small> Director {{ item.director }}</small>
+            <br />
+          </h2>
+
+          <p><b-badge>Opening crawl</b-badge> {{ item.opening_crawl }}</p>
+        </div>
+      </div>
+    </section>
+    <footer class="starship-details-footer">
+      <h2>Swapi Dev</h2>
+    </footer>
+  </div>
+  <!-- <div>
     <b-container>
       <div
         header="Battleship Details page"
@@ -123,23 +162,41 @@
         </div>
       </div>
     </b-container>
-  </div>
+  </div> -->
 </template>
 <script>
 import AddToCart from '../common/AddToCart.vue';
 import NavBar from '../NavBar';
+import ScrollMagic from 'scrollmagic';
 
 export default {
   components: { 'add-to-cart-buttton': AddToCart, 'app-nav': NavBar },
   props: ['id'],
+  data() {
+    return {
+      moviesLength: ''
+    };
+  },
+
+  // Use a Watch property maybe for the magic scroll length ?
   computed: {
     getbattleShipInfo() {
       return this.$store.getters['battleships/getBattleShipDetails'](this.id);
+      console.log(this.moviesLength);
     },
     getbattleShipMovies() {
       return this.$store.getters['battleships/getbattleShipMovieDetails'];
-      console.log(this.getbattleShipInfo);
     }
+  },
+  mounted() {
+    const controller = new ScrollMagic.Controller();
+    new ScrollMagic.Scene({
+      duration: '200%',
+      triggerElement: '#trigger-magic-scroll', //scroll distance for scene
+      triggerHook: 0
+    })
+      .setPin('#trigger-magic-scroll') //pins the element for the scenes duration
+      .addTo(controller);
   }
 };
 </script>
